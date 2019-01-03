36379fc82c7f727ea46281999b583f0a5e315e75
zzh1991 <zzh19910530@163.com>
2019-01-03 09:38:01 +0800

    add license and readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Java Utils
+
+## ListUtil
+- isListNullOrEmpty
+- splitListToSmallLists
+- splitStringToList
+- joinListToString
+- addAllToList
+- concat
+- union
+- intersection
+- difference
+
+## StringUtil
+- capitalizeEveryWord
+
+## DebugUtil
+- isDebugMode
+
+## TreadUtil
+- getExecutorService
---
+++ src/util/ThreadUtil.java
@@ -0,0 +1,24 @@
+package util;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadUtil {
+    private static ExecutorService executorService = null;
+
+    public ExecutorService getExecutorService() {
+        if (Objects.isNull(executorService)) {
+            executorService = new ThreadPoolExecutor(
+                    2, 4,
+                    60, TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<>(),
+                    new ThreadFactoryBuilder().setNameFormat("common-thread-%d").build());
+        }
+        return executorService;
+    }
+}
